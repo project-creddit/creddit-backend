67ae8649ed3f4d31522f800d6149da71feb86ae3
oereo <dlstpgns0406@gmail.com>
2022-03-26 20:50:50 +0900

    chore: initial commit

---
+++ src/main/java/com/creddit/credditmainserver/CredditMainServerApplication.java
@@ -0,0 +1,13 @@
+package com.creddit.credditmainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredditMainServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredditMainServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/creddit/credditmainserver/CredditMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creddit.credditmainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredditMainServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
