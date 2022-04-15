134734c6fe05e1c9f55fc94d885510caf1334d2b
kyh1685 <qnvnfls@naver.com>
2022-04-15 23:05:02 +0900

    refactor: PostRequestDto 병합 후 기존 Dto가 사용된 코드 변경

--- src/main/java/com/creddit/credditmainserver/service/PostService.java
+++ src/main/java/com/creddit/credditmainserver/service/PostService.java
@@ -2,8 +2,7 @@ package com.creddit.credditmainserver.service;
 
 import com.creddit.credditmainserver.domain.Member;
 import com.creddit.credditmainserver.domain.Post;
-import com.creddit.credditmainserver.dto.request.PostSaveRequestDto;
-import com.creddit.credditmainserver.dto.request.PostUpdateRequestDto;
+import com.creddit.credditmainserver.dto.request.PostRequestDto;
 import com.creddit.credditmainserver.dto.response.PostResponseDto;
 import com.creddit.credditmainserver.login.security.SecurityUtil;
 import com.creddit.credditmainserver.repository.MemberRepository;
@@ -24,19 +23,19 @@ public class PostService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long createPost(PostSaveRequestDto postSaveRequestDto){
+    public Long createPost(PostRequestDto postRequestDto){
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         Member member = memberRepository.getById(currentMemberId);
 
-        return postRepository.save(postSaveRequestDto.toEntity(member)).getId();
+        return postRepository.save(postRequestDto.toEntity(member)).getId();
     }
 
     @Transactional
-    public Long updatePost(Long id, PostUpdateRequestDto postUpdateRequestDto) {
+    public Long updatePost(Long id, PostRequestDto postRequestDto) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("게시글이 없습니다. id = " + id));
 
-        post.updatePost(postUpdateRequestDto.getTitle(), postUpdateRequestDto.getContent(), postUpdateRequestDto.getImgName());
+        post.updatePost(postRequestDto.getTitle(), postRequestDto.getContent(), postRequestDto.getImgName());
 
         return id;
     }
