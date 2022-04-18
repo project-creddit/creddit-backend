fcc4ec294e5f9ee4a327d17e2d1c3c28d2d563e8
kyh1685 <qnvnfls@naver.com>
2022-04-18 11:18:45 +0900

    fix: 테스트 오류 임시 수정

--- src/test/java/com/creddit/credditmainserver/service/PostServiceTest.java
+++ src/test/java/com/creddit/credditmainserver/service/PostServiceTest.java
@@ -3,8 +3,7 @@ package com.creddit.credditmainserver.service;
 import com.creddit.credditmainserver.domain.Authority;
 import com.creddit.credditmainserver.domain.Member;
 import com.creddit.credditmainserver.domain.Post;
-import com.creddit.credditmainserver.dto.request.PostSaveRequestDto;
-import com.creddit.credditmainserver.dto.request.PostUpdateRequestDto;
+import com.creddit.credditmainserver.dto.request.PostRequestDto;
 import com.creddit.credditmainserver.repository.MemberRepository;
 import com.creddit.credditmainserver.repository.PostRepository;
 import org.junit.jupiter.api.Test;
@@ -89,8 +88,7 @@ class PostServiceTest {
     }
 
     private Long createPost(Member member, String title, String content) {
-        return postService.createPost(PostSaveRequestDto.builder()
-                .member(member)
+        return postService.createPost(PostRequestDto.builder()
                 .title(title)
                 .content(content)
                 .build());
