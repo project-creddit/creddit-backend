f7b595719f36487129302e8f8581d7f006930b2f
oeunz <oeunzzang@naver.com>
2022-04-23 16:40:20 +0900

    feature: 닉네임 변경 기능 추가
    닉네임 변경

--- src/main/java/com/creddit/credditmainserver/api/MemberApiController.java
+++ src/main/java/com/creddit/credditmainserver/api/MemberApiController.java
@@ -1,14 +1,14 @@
 package com.creddit.credditmainserver.api;
 
+import com.creddit.credditmainserver.dto.response.MemberResponseDto;
 import com.creddit.credditmainserver.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Api(tags = {"회원가입 시 중복 체크"})
 @RestController
@@ -22,6 +22,12 @@ public class MemberApiController {
     @GetMapping("/checkDuplicateByEmail/{email}")
     public boolean checkDuplicateByEmail(@PathVariable String email){
         return memberService.checkDuplicateByEmail(email);
+    }
+
+    @Operation(summary="닉네임 변경", description = "닉네임 중복 확인 후 변경 필요")
+    @PostMapping("/changeNickname")
+    public MemberResponseDto changeNickname(@RequestBody String nickname, Principal principal){
+        return memberService.changeNickname(nickname,Long.parseLong(principal.getName()));
     }
 
     @Operation(summary="회원가입 시 닉네임 중복 체크",description="중복 시 return true, 중복 아닐 시 return false")
