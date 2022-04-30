f48835f5c76673242a5882dbe4f99751da497ec6
kyh1685 <qnvnfls@naver.com>
2022-04-30 14:45:20 +0900

    feat: 댓글 조회 기능 추가
    - 무한 스크롤
    - 정렬 기준(최신, 좋아요 순)

--- src/main/java/com/creddit/credditmainserver/api/CommentApiController.java
+++ src/main/java/com/creddit/credditmainserver/api/CommentApiController.java
@@ -1,12 +1,16 @@
 package com.creddit.credditmainserver.api;
 
 import com.creddit.credditmainserver.dto.request.CommentRequestDto;
+import com.creddit.credditmainserver.dto.response.CommentResponseDto;
 import com.creddit.credditmainserver.service.CommentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = {"댓글 작성/수정/삭제"})
 @RequiredArgsConstructor
@@ -14,6 +18,23 @@ import org.springframework.web.bind.annotation.*;
 public class CommentApiController {
 
     private final CommentService commentService;
+
+    @ApiOperation(value = "댓글 조회")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "postId", value = "글 ID"),
+            @ApiImplicitParam(name = "lastPostId", value = "마지막 댓글의 ID"),
+            @ApiImplicitParam(name = "size", value = "불러올 댓글의 개수"),
+            @ApiImplicitParam(name = "sort", value = "정렬 기준 ex) new, like")
+    })
+    @GetMapping("/comment")
+    public List<CommentResponseDto> getCommentPage(
+            @RequestParam Long postId,
+            @RequestParam Long lastCommentId,
+            @RequestParam int size,
+            @RequestParam String sort
+    ){
+        return commentService.fetchCommentPagesBy(postId, lastCommentId, size, sort);
+    }
 
     @ApiOperation(value = "댓글 작성", notes = "글 번호, 내용 필수값 / 내용 null, '', ' ' 모두 불가능")
     @ApiImplicitParam(name = "requestDto", value = "글 ID, 부모 댓글 ID, 내용")
--- src/main/java/com/creddit/credditmainserver/repository/CommentRepository.java
+++ src/main/java/com/creddit/credditmainserver/repository/CommentRepository.java
@@ -1,7 +1,19 @@
 package com.creddit.credditmainserver.repository;
 
 import com.creddit.credditmainserver.domain.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByIdLessThanAndPostIdOrderByIdDesc(Long id, Long postId, Pageable pageable);
+
+    @Query("select c from Comment c left join c.likes l left join c.post p " +
+            "where c.id < :id and p.id = :postId " +
+            "group by p.id, c.id, l.comment " +
+            "order by count(l.comment) desc, c.id desc")
+    Page<Comment> findByPageOfLikes(@Param("postId") Long postId, @Param("id") Long id, Pageable pageable);
 }
