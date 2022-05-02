fd6135dc09cdbc5b90e2e478ee20eedd7e0e81c8
oereo <dlstpgns0406@gmail.com>
2022-05-02 19:56:12 +0900

    feat: findBySearch method 추가

--- src/main/java/com/creddit/credditmainserver/config/QuerydslConfiguration.java
+++
@@ -1,2 +0,0 @@
-package com.creddit.credditmainserver.config;public class QuerydslConfiguration {
-}
--- src/main/java/com/creddit/credditmainserver/repository/MemberRepository.java
+++ src/main/java/com/creddit/credditmainserver/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.creddit.credditmainserver.repository;
 
 import com.creddit.credditmainserver.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +18,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
     @Query("SELECT m FROM Member m WHERE m.nickname = :nickname")
     Optional<Member> findByNickname(@Param("nickname") String nickname);
-
+    @Query("select m from Member m " +
+            "where m.nickname like %:keyword% or m.email like %:keyword% " +
+            "order by m.id desc")
+    Page<Member> findBySearch(@Param("keyword") String keyword, Pageable pageable);
 }
