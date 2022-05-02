9abb6c911c87e123f93b4e3dfdaaf2ff57c8c564
oereo <dlstpgns0406@gmail.com>
2022-05-02 19:07:53 +0900

    feat: MemberSearch Repository 추가

---
+++ src/main/java/com/creddit/credditmainserver/repository/MemberSearch.java
@@ -0,0 +1,12 @@
+package com.creddit.credditmainserver.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberSearch {
+
+    private String nickName;
+    private String email;
+}
