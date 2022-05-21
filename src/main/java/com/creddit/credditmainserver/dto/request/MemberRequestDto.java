5caafc5fbf7de8c4f85ab68517d80a8b69faabe0
oeunz <oeunzzang@naver.com>
2022-05-21 19:33:12 +0900

    fix: 회원가입 시 닉네임에 대문자가 입력되도록 수정
     회원가입 시 닉네임에 대문자가 입력되도록 수정

--- src/main/java/com/creddit/credditmainserver/dto/request/MemberRequestDto.java
+++ src/main/java/com/creddit/credditmainserver/dto/request/MemberRequestDto.java
@@ -21,7 +21,7 @@ public class MemberRequestDto {
     private String password;
 
     @NotEmpty(message = "닉네임을 입력해야 합니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
 
     public UsernamePasswordAuthenticationToken toAuthentication(){
